119005103c1e08716abc70a6a55c2e69bd4e2039
Lena Zamnius <lena.zamnius@gmail.com>
2020-03-12 15:09:08 +0200

    refactor: change overlay pop-up module

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const IMAGE_GALLERY = document.getElementById('image-container');
 const SUBMIT = document.getElementById('submit');
 const POP_UP = document.getElementById('pop-up');
 const POP_UP_BUTTON = document.getElementById('pop-up__button');
-const NAME_INPUT = document.getElementById('user_name');
-const EMAIL_INPUT = document.getElementById('email');
 const SUBJECT_INPUT = document.getElementById('subject');
 const DESCRIPTION_INPUT = document.getElementById('text');
 const POP_UP_SUBJECT = document.getElementById('ms_subject');
@@ -28,24 +26,21 @@ IMAGE_GALLERY.addEventListener('click', (event) => {
   event.target.classList.add('active');
 });
 
-POP_UP_BUTTON.addEventListener('click', () => {
-  NAME_INPUT.value = '';
-  EMAIL_INPUT.value = '';
-  SUBJECT_INPUT.value = '';
-  DESCRIPTION_INPUT.value = '';
-  POP_UP_SUBJECT.innerText = 'none';
-  POP_UP_DESCRIPTION.innerText = 'none';
-  POP_UP.classList.add('hidden');
-});
-
-SUBMIT.addEventListener('click', (e) => {
+SUBMIT.addEventListener('click', () => {
   const regex = /\S+@\S+/;
-  if(NAME_INPUT.value != '' && regex.test(EMAIL_INPUT.value)) {
+  if(document.getElementById('user_name').value != '' && regex.test(document.getElementById('email').value)) {
     if(SUBJECT_INPUT.value || DESCRIPTION_INPUT.value) {
       POP_UP_SUBJECT.innerText = SUBJECT_INPUT.value;
       POP_UP_DESCRIPTION.innerText = DESCRIPTION_INPUT.value;
     }
     POP_UP.classList.remove('hidden');
   }
+});
+
+POP_UP_BUTTON.addEventListener('click', () => {
+  document.querySelector('form').reset();
+  POP_UP_SUBJECT.innerText = 'none';
+  POP_UP_DESCRIPTION.innerText = 'none';
+  POP_UP.classList.add('hidden');
 });
 
