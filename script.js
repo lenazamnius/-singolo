65911f0b92049cc762a76a3d0b1f0ab819551168
Lena Zamnius <lena.zamnius@gmail.com>
2020-03-13 21:12:36 +0200

    feat: connect tabs with sort images in portfolio section

--- script.js
+++ script.js
@@ -9,23 +9,55 @@ const DESCRIPTION_INPUT = document.getElementById('text');
 const POP_UP_SUBJECT = document.getElementById('ms_subject');
 const POP_UP_DESCRIPTION = document.getElementById('ms_text');
 
-
+// header section 
 MENU.addEventListener('click', (event) => {
   MENU.querySelectorAll('a.menu__item').forEach((el) => el.classList.remove('active'));
   event.target.classList.add('active');
 });
+// header section end
 
+// slider section
+
+// slider section end
+
+// portfolio section
 SORT_BAR.addEventListener('click', (event) => {
   IMAGE_GALLERY.querySelectorAll('div.image-container__item > img').forEach((el) => el.classList.remove('active'));
   SORT_BAR.querySelectorAll('a.sort-bar__button').forEach((el) => el.classList.remove('active'));
+  const imagesArrLength =  document.querySelectorAll('.image-container__item').length;
+  const indexesArr = Array.from({length:imagesArrLength}, (num1, num2) => num2 + 1);
+  if(document.getElementById('web_btn').isSameNode(event.target)) {
+    const evenIndexes = indexesArr.filter(a => a % 2 === 0);
+    const oddIndexes = indexesArr.filter(a => a % 2 !== 0);
+    const evenOddArr = [...evenIndexes, ...oddIndexes];
+    indexesPattern(evenOddArr);
+  } else if(document.getElementById('graphic_btn').isSameNode(event.target)) {
+    const shuffledIndexesArr = indexesArr.sort((a, b) => -1 + Math.random() * 3);
+    indexesPattern(shuffledIndexesArr);
+  } else if(document.getElementById('art_btn').isSameNode(event.target)) {
+    const reversedIndexesArr = indexesArr.reverse();
+    indexesPattern(reversedIndexesArr);
+  } else {
+    indexesPattern(indexesArr);
+  }
   event.target.classList.add('active');
 });
+
+function indexesPattern(arr) {
+  for(i = 0; i < arr.length; i++) {
+    let tempNode = (document.getElementById(`image${arr[i]}`));
+    IMAGE_GALLERY.removeChild((document.getElementById(`image${arr[i]}`)));
+    IMAGE_GALLERY.appendChild(tempNode);
+  }
+}
 
 IMAGE_GALLERY.addEventListener('click', (event) => {
   IMAGE_GALLERY.querySelectorAll('div.image-container__item > img').forEach((el) => el.classList.remove('active'));
   event.target.classList.add('active');
 });
+// portfolio section end
 
+// contacts section
 SUBMIT.addEventListener('click', () => {
   const regex = /\S+@\S+/;
   if(document.getElementById('user_name').value != '' && regex.test(document.getElementById('email').value)) {
@@ -43,4 +75,5 @@ POP_UP_BUTTON.addEventListener('click', () => {
   POP_UP_DESCRIPTION.innerText = 'none';
   POP_UP.classList.add('hidden');
 });
+// contacts section end
 
