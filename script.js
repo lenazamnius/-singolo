a94ecd45aa1fc9faf714427e8a75bd43404f8dd7
Lena Zamnius <lena.zamnius@gmail.com>
2020-03-14 13:48:25 +0200

    feat: add activation phone screens in slider

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-const MENU = document.getElementById('menu');
-const SORT_BAR = document.getElementById('sort-bar');
-const IMAGE_GALLERY = document.getElementById('image-container');
-const SUBMIT = document.getElementById('submit');
-const POP_UP = document.getElementById('pop-up');
-const POP_UP_BUTTON = document.getElementById('pop-up__button');
-const SUBJECT_INPUT = document.getElementById('subject');
-const DESCRIPTION_INPUT = document.getElementById('text');
-const POP_UP_SUBJECT = document.getElementById('ms_subject');
-const POP_UP_DESCRIPTION = document.getElementById('ms_text');
+const menu = document.getElementById('menu');
+const sortBar = document.getElementById('sort-bar');
+const imageGallery = document.getElementById('image-container');
+const submit = document.getElementById('submit');
+const popUp = document.getElementById('pop-up');
+const popUpButton = document.getElementById('pop-up__button');
+const subjectInput = document.getElementById('subject');
+const descriptionInput = document.getElementById('text');
+const popUpSubject = document.getElementById('ms_subject');
+const popUpDescription = document.getElementById('ms_text');
 
 // header section 
-MENU.addEventListener('click', (event) => {
-  MENU.querySelectorAll('a.menu__item').forEach((el) => el.classList.remove('active'));
+menu.addEventListener('click', (event) => {
+  menu.querySelectorAll('a.menu__item').forEach((el) => el.classList.remove('active'));
   event.target.classList.add('active');
 });
 // header section end
 
 // slider section
+const verticalPhone = document.getElementById('vertical-phone');
+const horizontalPhone = document.getElementById('horizontal-phone');
+
+verticalPhone.addEventListener('click', () => {
+  let verDisplay = document.getElementById('vertical-display');
+  (verDisplay.style.zIndex === '' || verDisplay.style.zIndex === '5') ? verDisplay.style.zIndex = '3' : verDisplay.style.zIndex = '5';
+});
+
+horizontalPhone.addEventListener('click', () => {
+  let horDisplay = document.getElementById('horizontal-display');
+  (horDisplay.style.zIndex === '' || horDisplay.style.zIndex === '5') ? horDisplay.style.zIndex = '3' : horDisplay.style.zIndex = '5';
+});
 
 // slider section end
 
 // portfolio section
-SORT_BAR.addEventListener('click', (event) => {
-  IMAGE_GALLERY.querySelectorAll('div.image-container__item > img').forEach((el) => el.classList.remove('active'));
-  SORT_BAR.querySelectorAll('a.sort-bar__button').forEach((el) => el.classList.remove('active'));
+sortBar.addEventListener('click', (event) => {
+  imageGallery.querySelectorAll('div.image-container__item > img').forEach((el) => el.classList.remove('active'));
+  sortBar.querySelectorAll('a.sort-bar__button').forEach((el) => el.classList.remove('active'));
   const imagesArrLength =  document.querySelectorAll('.image-container__item').length;
   const indexesArr = Array.from({length:imagesArrLength}, (num1, num2) => num2 + 1);
   if(document.getElementById('web_btn').isSameNode(event.target)) {
@@ -46,34 +58,34 @@ SORT_BAR.addEventListener('click', (event) => {
 function indexesPattern(arr) {
   for(i = 0; i < arr.length; i++) {
     let tempNode = (document.getElementById(`image${arr[i]}`));
-    IMAGE_GALLERY.removeChild((document.getElementById(`image${arr[i]}`)));
-    IMAGE_GALLERY.appendChild(tempNode);
+    imageGallery.removeChild((document.getElementById(`image${arr[i]}`)));
+    imageGallery.appendChild(tempNode);
   }
 }
 
-IMAGE_GALLERY.addEventListener('click', (event) => {
-  IMAGE_GALLERY.querySelectorAll('div.image-container__item > img').forEach((el) => el.classList.remove('active'));
+imageGallery.addEventListener('click', (event) => {
+  imageGallery.querySelectorAll('div.image-container__item > img').forEach((el) => el.classList.remove('active'));
   event.target.classList.add('active');
 });
 // portfolio section end
 
 // contacts section
-SUBMIT.addEventListener('click', () => {
+submit.addEventListener('click', () => {
   const regex = /\S+@\S+/;
   if(document.getElementById('user_name').value != '' && regex.test(document.getElementById('email').value)) {
-    if(SUBJECT_INPUT.value || DESCRIPTION_INPUT.value) {
-      POP_UP_SUBJECT.innerText = SUBJECT_INPUT.value;
-      POP_UP_DESCRIPTION.innerText = DESCRIPTION_INPUT.value;
+    if(subjectInput.value || descriptionInput.value) {
+      popUpSubject.innerText = subjectInput.value;
+      popUpDescription.innerText = descriptionInput.value;
     }
-    POP_UP.classList.remove('hidden');
+    popUp.classList.remove('hidden');
   }
 });
 
-POP_UP_BUTTON.addEventListener('click', () => {
+popUpButton.addEventListener('click', () => {
   document.querySelector('form').reset();
-  POP_UP_SUBJECT.innerText = 'none';
-  POP_UP_DESCRIPTION.innerText = 'none';
-  POP_UP.classList.add('hidden');
+  popUpSubject.innerText = 'none';
+  popUpDescription.innerText = 'none';
+  popUp.classList.add('hidden');
 });
 // contacts section end
 
